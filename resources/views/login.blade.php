e403139e0bda6d1c6376cd3a3bc04ec69fdc009a
Melkias Teshoma <melkiasapo@gmail.com>
2025-08-08 19:51:51 +0300

    This Tarvel Blog done by PHP where you share your exprience when your visition time

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body class="text-4xl font-bold text-green-600">
+  <h1 class="text-4xl font-bold text-green-600">Login</h1>
+  <form action="/login">
+  <input type="text" name="" id="">
+  <input type="text" name="" id="">
+  <button>Login</button>
+  </form>
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>register</h1>
+  <form action="/register" method="POST">
+  
+  <input type="text" name="" id="">
+  <input type="text" name="" id="">
+  <input type="text" name="" id="">
+  <input type="text" name="" id="">
+  <button>Register</button>
+  </form>
+</body>
+</html>
